67a21655fc17c2a42dab8e2809da6a47a8a50eee
aaazqsde <maxime.fesselier@gmail.com>
2017-03-07 17:59:45 +0100

    add bubbleV2
    jolie bubble

---
+++ bubbleV2/bubble.js
@@ -0,0 +1,24 @@
+function Bubble(x, y) {
+    this.x = x;
+    this.y = y;
+    this.diameter = random(10, 120);
+    this.trans = random(10, 200);
+    this.r = random(0, 255);
+    this.g = random(0, 255);
+    this.b = random(0, 255);
+    this.sx = random(-1, 1);
+    this.sy = random(-1, 1);
+
+    this.display = function() {
+        noStroke();
+        //noFill();
+        fill(this.r, this.g, this.b, this.trans);
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+
+    this.move = function() {
+        this.x += this.sx;
+        this.y += this.sy;
+    }
+
+}
